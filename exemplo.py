ff7d9381d6e0795648a33870a7212fa44b9cf56d
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-10-10 20:33:42 -0300

    exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Criando banco de dados.
 db = create_engine("sqlite:///meubanco.db")
 
-# Conexão com o banco de dados. 
+# Conexão com o banco de dados.
 Session = sessionmaker(bind=db)
 session = Session()
 
@@ -13,57 +13,56 @@ session = Session()
 Base = declarative_base()
 
 class Usuario(Base):
-    # Definindo atributos da tabela.
+    # Definindo nome da tabela.
     __tablename__ = "usuarios"
 
     # Definindo atributos da tabela.
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome", String)
-    email = Column("email",  String)
+    email = Column("email", String)
     senha = Column("senha", String)
 
     # Definindo atributos da classe.
-    def __init__(self, nome: str, email: str, senha: str) -> None:
+    def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome
         self.email = email
         self.senha = senha
 
-# Criando tabela banco de dados.
+# Criando tabela no banco de dados.
 Base.metadata.create_all(bind=db)
 
-# Salvar no banco de dados.
-# usuario = Usuario(nome="Marta", email="marta@gmail.com", senha="123")
-# session.add(usuario)
-# session.commit()
+os.system("cls || clear")
 
-for i in range(2): 
+# Salvar no banco de dados.
+# usuario = Usuario("Marta", "marta@gmail.com", "123")
+# usuario = Usuario(senha="123", nome="José", email="jose@gmail.com")
+
+for i in range(2):
     nome = input("Digite seu nome: ")
     email = input("Digite seu e-mail: ")
-    senha = input("Digite sua senha: ")
+    senha=  input("Digite sua senha: ")
 
-    usuario = Usuario(senha=senha, nome=nome, email=email)
+    usuario =  Usuario(senha=senha, nome=nome, email=email)
     session.add(usuario)
     session.commit()
     print()
 
-#  Mostrando conteúdo do banco de dados.
-print("\nListando usuários do banco de dados.")
+# Mostrando conteúdo do banco de dados.
+print("\nListando usuários no banco de dados.")
 lista_usuarios = session.query(Usuario).all()
 
-for usuario in lista_usuarios: 
+for usuario in lista_usuarios:
     print(f"{usuario.id} - {usuario.nome} - {usuario.email}")
 
-# Deletando um usuário. 
-usuario = session.query(Usuario).filter_by(email="joao@gmail.com").first()
+# Deletando um usuário.
+usuario = session.query(Usuario).filter_by(email ="joao@gmail.com").first()
 session.delete(usuario)
 session.commit()
 print("\nUsuário deletado com sucesso.")
+
+# Mostrando conteúdo do banco de dados.
+print("\nListando usuários no banco de dados.")
 lista_usuarios = session.query(Usuario).all()
 
-print("\nListando usuários do banco de dados.")
-lista_usuarios = session.query(Usuario).all()
-
-for usuario in lista_usuarios: 
+for usuario in lista_usuarios:
     print(f"{usuario.id} - {usuario.nome} - {usuario.email}")
-
-
